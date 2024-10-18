ceb3ed2dbf13c32678642fbf76c07b80c6003bbe
deb-cod <pramanickdebesh@gmail.com>
2024-10-18 15:34:30 +0530

    format fixes 3

--- docker/standalone.php
+++ docker/standalone.php
@@ -734,16 +734,12 @@
 	</script>
 	<script>
 		window.embeddedChatbotConfig = {
-		chatbotId: "-bDQdBAoEWAettWmMb2-C",
-		domain: "www.chatbase.co"
+			chatbotId: "-bDQdBAoEWAettWmMb2-C",
+			domain: "www.chatbase.co"
 		};
-    </script>
-    <script
-		src="https://www.chatbase.co/embed.min.js"
-		chatbotId="-bDQdBAoEWAettWmMb2-C"
-		domain="www.chatbase.co"
-		defer>
-    </script>
+	</script>
+	<script src="https://www.chatbase.co/embed.min.js" chatbotId="-bDQdBAoEWAettWmMb2-C" domain="www.chatbase.co" defer>
+	</script>
 </body>
 
 </html>
